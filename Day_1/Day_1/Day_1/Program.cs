1ada1fd56a431eb22406d6df79638b660aa8a690
lindakolkovska <42386003+lindakolkovska@users.noreply.github.com>
2018-09-03 19:48:48 +0300

    Day 1 created

---
+++ Day_1/Day_1/Day_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WritenLine("Hello World!");
+            int VeselsSkaitlis = 5;
+            double arKomatu = 4.5;
+            String simbouVirkne = "definejas sadi";
+            char simbols = 'g';
+            bool patiessVaiNee = true;
+
+            VeselsSkaitlis = 6;
+            simboluVirkne = Console.ReadLine();
+            Console.WriteLine(VeselsSkaitlis);
+
+            Console.ReadLine();
+        }
+    }
+}
